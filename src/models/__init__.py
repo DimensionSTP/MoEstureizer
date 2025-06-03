ae8feee76538132e2a910092eaafff090c4497d3
DimensionSTP <ddang8jh@gmail.com>
2025-06-03 16:51:17 +0900

    feat: import MoEsturizedLlamaForCausalLM

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,17 +1,5 @@
-from .moff import MixtureOfFeedForward
-from .moff import MixtureOfInterFeedForward
-from .lm_head_router import LMHeadRouter
-from .lm_head_router import LMHeadGatedRouter
-from .modeling_mix_llama import MixLlamaForCausalLM
-from .modeling_deepmix_llama import DeepMixLlamaForCausalLM
-from .modeling_gated_mix_llama import GatedMixLlamaForCausalLM
+from .modeling_moesturized_llama import MoEsturizedLlamaForCausalLM
 
 __all__ = [
-    "MixtureOfFeedForward",
-    "MixtureOfInterFeedForward",
-    "LMHeadRouter",
-    "LMHeadGatedRouter",
-    "MixLlamaForCausalLM",
-    "DeepMixLlamaForCausalLM",
-    "GatedMixLlamaForCausalLM",
+    "MoEsturizedLlamaForCausalLM",
 ]
