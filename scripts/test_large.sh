81987b8ba201185c6b3bc15e584b0bf104ea1879
DimensionSTP <ddang8jh@gmail.com>
2025-06-13 16:15:01 +0900

    add: default test_large scripts

---
+++ scripts/test_large.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+data_type="conversational"
+dataset_name="mmlu"
+is_preprocessed=False
+moe_type="moesturized"
+initialize=False
+base_path="/data/MoEstureizer"
+model_detail="Qwen3-8B-${moe_type}-experts_8-tok_3"
+pretrained_model_name="${base_path}/${model_detail}"
+left_padding=True
+max_length=2048
+max_new_tokens=256
+eval_batch_size=16
+workers_ratio=8
+use_all_workers=False
+
+python main.py mode=test_large \
+    data_type=$data_type \
+    dataset_name=$dataset_name \
+    is_preprocessed=$is_preprocessed \
+    moe_type=$moe_type \
+    initialize=$initialize \
+    model_detail=$model_detail \
+    pretrained_model_name=$pretrained_model_name \
+    left_padding=$left_padding \
+    max_length=$max_length \
+    max_new_tokens=$max_new_tokens \
+    eval_batch_size=$eval_batch_size \
+    workers_ratio=$workers_ratio \
+    use_all_workers=$use_all_workers
---
+++ scripts/test_large_all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+data_type="conversational"
+
+dataset_names=(
+    "aime24"
+    "arc_challenge"
+    "arc_easy"
+    "gpqa_diamond"
+    "gpqa_extended"
+    "gpqa_main"
+    "gsm8k"
+    "humaneval"
+    "math500"
+    "mbpp"
+    "mmlu"
+    "mmlu_pro"
+    "open_rewrite_eval"
+)
+
+is_preprocessed=False
+moe_type="moesturized"
+initialize=False
+base_path="/data/MoEstureizer"
+
+model_details=(
+    "Qwen3-8B-${moe_type}-experts_8-tok_3"
+    "Qwen3-14B-${moe_type}-experts_8-tok_3"
+    "Qwen3-32B-${moe_type}-experts_8-tok_3"
+)
+
+left_padding=True
+max_length=2048
+max_new_tokens=256
+eval_batch_size=16
+workers_ratio=8
+use_all_workers=False
+
+for model_detail in "${model_details[@]}"
+do
+    pretrained_model_name="${base_path}/${model_detail}"
+
+    for dataset_name in "${dataset_names[@]}"
+    do
+        echo "Running evaluation on dataset: $dataset_name"
+
+        python main.py mode=test_large \
+            data_type=$data_type \
+            dataset_name=$dataset_name \
+            is_preprocessed=$is_preprocessed \
+            moe_type=$moe_type \
+            initialize=$initialize \
+            model_detail=$model_detail \
+            pretrained_model_name=$pretrained_model_name \
+            left_padding=$left_padding \
+            max_length=$max_length \
+            max_new_tokens=$max_new_tokens \
+            eval_batch_size=$eval_batch_size \
+            workers_ratio=$workers_ratio \
+            use_all_workers=$use_all_workers
+    done
+done
